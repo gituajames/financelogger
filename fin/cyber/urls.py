946702820130101b0b5f1702da33f770ee930f31
gitua <gitua@localhost.localdomain>
2025-03-21 15:19:24 +0300

    altered unwatched migrations

--- fin/cyber/urls.py
+++ fin/cyber/urls.py
@@ -2,9 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
-    # path("finlogger/", include("finlogger.urls")),
-    path('', views.index, name='index'),
-    path('home', views.home, name='home'),
+    path('index/', views.index, name='index'),
+    path('home/', views.home, name='home'),
     # path('inventory/', include("inventory.urls"))
 ]
