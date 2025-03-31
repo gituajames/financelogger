3a104d0e75b2bbfc62f9a60a5f978d534a3ab8b4
gitua <gitua@localhost.localdomain>
2025-03-31 12:01:40 +0300

    still modifiying urls for static

--- fin/fin/urls.py
+++ fin/fin/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("finlogger/", include("finlogger.urls")),
     path('cyber/', include("cyber.urls"),)
     # path('inventory/', include("inventory.urls"))
-]
+] + static(setting.STATIC_URL, document_root=settings.STATIC_ROOT)
