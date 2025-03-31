e29b3bca283a6bb13c020dec712e787ead165202
gitua <gitua@localhost.localdomain>
2025-03-31 11:50:34 +0300

    changed path to static files

--- fin/fin/settings.py
+++ fin/fin/settings.py
@@ -123,7 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = "/home/gituajames/financelogger/fin/static"
 
 
 # Default primary key field type
