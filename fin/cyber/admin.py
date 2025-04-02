a72707726d686184093f394cae6255cda473ba25
gitua <gitua@localhost.localdomain>
2025-04-03 00:52:02 +0300

    added model choices for services hope it wont raise issues like last time

--- fin/cyber/admin.py
+++ fin/cyber/admin.py
@@ -4,7 +4,7 @@ from .models import Service, Expenses
 
 @admin.register(Service)
 class ServiceAdmin(admin.ModelAdmin):
-    list_display = ('service', 'price', 'count', 'description', 'date')
+    list_display = ('service', 'price', 'count', 'description', 'date', 'paid_status')
 # admin.site.register(Service)
 
 
--- fin/cyber/models.py
+++ fin/cyber/models.py
@@ -1,11 +1,35 @@
 from django.db import models
 
+service_choices = {
+    "printing" : "Printing",
+    "photocopy" : "Photocopy",
+    "scanning" : "Scanning",
+    "kra" : "KRA",
+    "SHA / SHIF" : "SHA/SHIF",
+    "emails": "mail services",
+    "browsing": "Browsing",
+    "passports" : "Passport Photo",
+    "ntsa" : "NTSA",
+    "HELB" : "HELB",
+    "Sale" : "Sales",
+    "lamination" : "Lamination",
+    "typsetting" : "Typesetting",
+    "songs" : "Songs",
+    "tse": "TSE",
+    "mpesa statements" : "M-pesa statements",
+    "pension" : "Pension",
+
+    "others": "others",
+
+}
+
 class Service(models.Model):
-    service = models.CharField(max_length=100)
+    service = models.CharField(max_length=100, choices=service_choices)
     price = models.IntegerField(default=0)
     count = models.IntegerField(default=0)
     description = models.CharField(max_length=200)
     date = models.DateTimeField(auto_now=True)
+    paid_status = models.BooleanField(default=False)
 
     def __str__(self):
         return self.service
